96bfc38d934b9fe1ac75cc85e4960a376dbd3439
Richard L King <rlking@aultan.com>
2017-01-30 19:02:29 +0000

    Fix getDayOfWeek.bat incorrect result on Sunday

--- resources/Scripts/getDayOfWeek.bat
+++ resources/Scripts/getDayOfWeek.bat
@@ -6,10 +6,10 @@
 
 for /f %%a in ('wmic path win32_localtime get DAYOFWEEK /format:list ^| findstr "="') do (set %%a)
 
+if "%DAYOFWEEK%" == "0" set DAYOFWEEK=SUNDAY
 if "%DAYOFWEEK%" == "1" set DAYOFWEEK=MONDAY
 if "%DAYOFWEEK%" == "2" set DAYOFWEEK=TUESDAY
 if "%DAYOFWEEK%" == "3" set DAYOFWEEK=WEDNESDAY
 if "%DAYOFWEEK%" == "4" set DAYOFWEEK=THURSDAY
 if "%DAYOFWEEK%" == "5" set DAYOFWEEK=FRIDAY
 if "%DAYOFWEEK%" == "6" set DAYOFWEEK=SATURDAY
-if "%DAYOFWEEK%" == "7" set DAYOFWEEK=SUNDAY
