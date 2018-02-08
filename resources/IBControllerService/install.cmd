9686c04fae9ea7539fb83489a795421c7ad68920
Richard L King <rlktradewright@users.noreply.github.com>
2018-02-08 22:42:05 +0000

    Remove IBControllerService files

--- resources/IBControllerService/install.cmd
+++
@@ -1,7 +0,0 @@
-﻿:: Batch file to install IBControllerService
-
-:: Runs "installUtil.exe" to install the service.
-C:\Windows\Microsoft.NET\Framework\v2.0.50727\installutil.exe c:\IBControllerService\bin\IBControllerService.exe
-
-:: To uninstall:
-:: C:\Windows\Microsoft.NET\Framework\v2.0.50727\installutil.exe /u c:\IBControllerService\bin\IBControllerService.exe
