e7d120cdcf893125c143650a503cfa5554bc90f8
Richard L King <rlktradewright@users.noreply.github.com>
2018-02-09 12:58:22 +0000

    Fix namespace identifier

--- src/twsautomater/ConfigurationTask.java
+++ src/twsautomater/ConfigurationTask.java
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with TwsAutomater.  If not, see <http://www.gnu.org/licenses/>.
 
-package TwsAutomater;
+package twsautomater;
 
 import java.util.concurrent.FutureTask;
 import javax.swing.JDialog;
--- src/twsautomater/TwsAutomaterException.java
+++ src/twsautomater/TwsAutomaterException.java
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with TwsAutomater.  If not, see <http://www.gnu.org/licenses/>.
 
-package TwsAutomater;
+package twsautomater;
 
 public class TwsAutomaterException extends Exception {
     private static final long serialVersionUID = 20140324L;
