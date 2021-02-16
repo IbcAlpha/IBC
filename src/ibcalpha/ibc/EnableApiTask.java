7c7ce2062011849ec4c0ca11977e11ed8355b7dc
Richard L King <rlktradewright@users.noreply.github.com>
2021-02-16 23:32:18 +0000

    Use selectApiSettings method

--- src/ibcalpha/ibc/EnableApiTask.java
+++ src/ibcalpha/ibc/EnableApiTask.java
@@ -35,9 +35,7 @@ class EnableApiTask implements ConfigurationAction{
         try {
             Utils.logToConsole("Doing ENABLEAPI configuration");
 
-            if (!Utils.selectConfigSection(configDialog, new String[] {"API","Settings"}))
-                // older versions of TWS don't have the Settings node below the API node
-                Utils.selectConfigSection(configDialog, new String[] {"API"});
+            Utils.selectApiSettings(configDialog);
 
             JCheckBox cb = SwingUtils.findCheckBox(configDialog, "Enable ActiveX and Socket Clients");
             if (cb == null) throw new IbcException("could not find Enable ActiveX checkbox");
