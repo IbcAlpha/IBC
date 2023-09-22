0e5cd4fd3a3fee17b972ce7acb0ce0f6c62e7c2d
Richard L King <rlktradewright@users.noreply.github.com>
2023-09-22 21:53:23 +0100

    Fix incorrect default TWS_MAJOR_VRSN variable for macOS start scripts

--- resources/gatewaystartmacos.sh
+++ resources/gatewaystartmacos.sh
@@ -18,7 +18,7 @@
 #=============================================================================+
 
 
-TWS_MAJOR_VRSN=1019
+TWS_MAJOR_VRSN=10.19
 IBC_INI=~/ibc/config.ini
 TRADING_MODE=
 TWOFA_TIMEOUT_ACTION=exit
